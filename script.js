b8fa16f9abda907e55bb846c9b95160286bc19d5
SNKD92 <67509474+SNKD92@users.noreply.github.com>
2021-08-13 23:56:01 +0300

    Update script.js
    First working version

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 'use strict';
 
-const gameNumber = '1000';
+const gameNumber = 3000;
+console.log(gameNumber)
 let cows = 0;
 let bulls = 0;
+let usernumber;
+const promt = function () {
+    userNumber = window.prompt(`Enter the 4 digit number: `);
+}
 
-// const validityCheck = function (number) {
-//   if (number.length == 4 && number.match(/^[0-9]+$/)) {
-//     return true;
-//   } else return false;
-// };
+promt();
+
+const bullCow = function (gameNumberinput, userNumberinput) {
+    for (let i = 0; i < gameNumberinput.length; i++) {
+      if (gameNumberinput[i] === userNumberinput[i]) {
+        bulls += 1;
+      } else cows += 1;
+    }
+};
+
 const validityCheck = function (number) {
   if (number.length == 4 && number.match(/^[0-9]+$/)) {
     return true;
   } else return false;
 };
 
-let userNumber = window.prompt('Enter the 4 digit number:');
+let userCorrectNumber = validityCheck(userNumber);
 
-const userCorrectNumber = validityCheck(userNumber);
+while (userCorrectNumber == false) {
+    promt();
+    userCorrectNumber = validityCheck(userNumber);
+    if(userCorrectNumber) {continue} else {
+        promt();
+        userCorrectNumber = validityCheck(userNumber);
+    };
+}
 
-// if (userCorrectNumber) {
-// } else {
-//   window.alert('Wrong number format try again!');
-//   userNumber = window.prompt(`Enter the 4 digit number: `);
-// }
-
-const bullCow = function (gameNumberinput, userNumberinput) {
-  for (let i = 0; i < gameNumberinput.length; i++) {
-    if (gameNumberinput[i] === userNumberinput[i]) {
-      bulls += 1;
-    } else cows += 1;
-  }
-};
-
-const result = bullCow(gameNumber, userNumber);
+bullCow(gameNumber, userNumber);
 window.alert(`You got ${bulls} bull/s and ${cows} cow/s!`);
